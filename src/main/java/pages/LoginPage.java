cded5579d77696c9499d7be806566de5e48a0f24
ILIA <brakerilya@yandex.ru>
2025-11-02 16:10:37 +0300

    create project and create positive test 2

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,27 @@
 package pages;
 
-public class LoginPage {
+import dto.User;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public class LoginPage extends BasePage {
+    public LoginPage(WebDriver driver){
+        setDriver(driver);
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 10), this);
+    }
+    @FindBy(id = "email")
+    WebElement inputEmail;
+    @FindBy(id = "password")
+    WebElement inputPassword;
+    @FindBy(xpath = "//button[@type=\"submit\"]")
+    WebElement btnYalla;
+    public void typeLoginForm(User user){
+        inputEmail.sendKeys(user.getUsername());
+        inputPassword.sendKeys(user.getPassword());
+        btnYalla.click();
+    }
+
 }
--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,4 +1,22 @@
 package ui_tests;
 
-public class LoginTests {
+import dto.User;
+import manager.ApplicationManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends ApplicationManager {
+    @BeforeMethod
+    public void goToLogin(){
+        new HomePage(getDriver()).clickBtnLoginHeader();
+    }
+
+    @Test
+    public void loginPositiveTest(){
+        User user = User.builder().username("bilbo_baggins_12345@mail.com").password("Password123!").build();
+        new LoginPage(getDriver()).typeLoginForm(user);
+    }
+
 }
